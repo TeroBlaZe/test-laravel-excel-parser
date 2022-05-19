f7972101ba179a4ce3b6e4b67603574afebf9104
Roman Smola <bronner888@gmail.com>
2022-05-19 16:16:57 +0300

    Add rows table migration

---
+++ app/Models/Row.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Row extends Model
+{
+    public $incrementing = false;
+    public $timestamps = false;
+    protected $guarded = [];
+    protected $casts = [
+        'date' => 'date',
+    ];
+}
